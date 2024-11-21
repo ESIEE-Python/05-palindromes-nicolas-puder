49ba32b8ecadf8a0629f14f9e0e802d73b47bc38
nicolas-puder <nicolas.puder@edu.esiee.fr>
2024-11-21 09:58:48 +0000

    exercie palindrome terminé

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
+"""
+Exercice de programation de la méthode ispalindrome()
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """
+    Permet de vérifier si la phrase ou le mot mis en paramètre est un palindrome
 
-    # votre code ici
-    
+        Args:
+            p: palindrome à vérifier
+
+        Returns:
+            True : p est un palindrome
+            False : p n'est pas un palindrome
+    """
+
+    x = "éèàçùïöüäîôâëê"
+    y = "eeacuiouaioaee"
+    z = " '-_,!?;:/()|}{[]#&"
+    modif = str.maketrans(x,y,z)
+    smin = p.lower()
+    scompare = smin.translate(modif)
+    spalin = scompare[::-1]
+
+    if spalin == scompare :
+        return True
     return False
 
 #### Fonction principale
 
 
 def main():
+    """
+    Testes pour la méthode ispalindrome()
+    """
 
     # vos appels à la fonction secondaire ici
+    print(ispalindrome("Je vais à l'ESIEE"))
+    print(ispalindrome("Rêver"))
+    print(ispalindrome("TnT"))
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
